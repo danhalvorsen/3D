0a25c00d6bcba2308a4e8d95810686e42b671d80
Dan Edvard Halvorsen <dan.halvorsen@bouvet.no>
2022-04-23 11:19:22 +0200

    more to inital commit

--- src/tests/create.indexed.indices.test.ts
+++ src/tests/create.indexed.indices.test.ts
@@ -1,14 +1,21 @@
 
 export const create = (size: number, segments: number) => {
    
-    let indices: Array<number> = [];
+    let vertices: Array<number> = [];
 
     const halfSize = size / 2;
     const segmentSize = size / segments;
 
     for (let i = 0; i < segments; i++) {
+        const y = ( i * segmentSize ) - halfSize;
 
+        for ( let j = 0; j <= segments; j ++ ) {
+            const x = ( j * segmentSize ) - halfSize;
+            vertices.push( x, - y, 0 );
+        }
     }
+
+    return vertices;
 };
 
 import jest from '@jest/globals'
@@ -19,4 +26,18 @@ describe('create', () => {
         const mesh = create(size, segments);
         expect(mesh).toBeDefined(); 
     });
-});
+
+    it('mesh with size 1 and with only 1 segment should have 6 vertices', () => {
+        const size = 1;
+        const segments = 1;
+        const mesh = create(size, segments); 
+        expect(mesh.length).toBe(6);
+    });
+
+    it('mesh with size 1 and with only 2 segment should have 6*4 (24) vertices', () => {
+        const size = 1;
+        const segments = 2;
+        const mesh = create(size, segments); 
+        expect(mesh.length).toBe(24);
+    });
+})
