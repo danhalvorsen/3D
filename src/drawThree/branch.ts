160cb8fdc8687454000cad1653119f37cda53c76
Dan Halvorsen <dehalvorsen@gmail.com>
2022-08-14 22:45:39 +0200

    new stuff. Still struggling with the usage

--- src/drawThree/branch.ts
+++ src/drawThree/branch.ts
@@ -14,52 +14,55 @@ export type BranchData = {
   length: number
 }
 
-export const branch = (branchData: BranchData): THREE.Mesh => {
+export const branch = (
+  branchData: BranchData,
+  scene: THREE.Scene,
+): THREE.Group => {
   const length = branchData.pos.start.vec.distanceTo(branchData.pos.end.vec)
 
   const geometry = new THREE.CylinderGeometry(
     branchData.radiusPair.startRadius,
     branchData.radiusPair.endRadius,
     length,
-    8,
+    36,
   )
 
-  return new THREE.Mesh(geometry, materials().basicMaterial)
+  const mesh = new THREE.Mesh(geometry, materials().basicMaterial)
+  mesh.add(new THREE.AxesHelper(15))
+  const group = new THREE.Group()
+  group.add(new THREE.AxesHelper(15))
+  group.add(mesh)
+
+  mesh.position.set(0.5, 0.5, 0)
+  group.position.set(-0.5, -0.5, 0)
+
+  scene.add(group)
+  return group
 }
 
 export const threeGenerator = (scene: THREE.Scene, branchData: BranchData) => {
-  const func = (branchData: BranchData, mother?: THREE.Mesh) => {
+  const func = (branchData: BranchData, mother?: THREE.Group) => {
     if (branchData.depth < 4) return
 
-    const mesh = branch(branchData)
-    scene.add(mesh)
-
-    mesh.add(new THREE.AxesHelper(15))
+    const meshGroup = branch(branchData, scene)
 
     if (mother === undefined) {
-      // var box = new THREE.Box3().setFromObject(mesh);
-      // box.getCenter(mesh.position); // this re-sets the mesh position
-      // mesh.position.multiplyScalar(-1);
+      const pos = meshGroup.position
 
-      const pivot = new THREE.Group()
-      pivot.position.set(0, 0, 0)
-      scene.add(pivot)
-      pivot.add(mesh)
-
-      const pos = mesh.position
-
-      console.log(`adding root@[${pos.x},${pos.y},${pos.z}]`)
+      console.log(
+        `adding root@[${pos.x},${pos.y},${pos.z}] length:${branchData.length} `,
+      )
     } else {
       const pos = new THREE.Vector3(
         branchData.pos.start.vec.x,
         branchData.pos.start.vec.y,
         branchData.pos.start.vec.z,
       )
-      mesh.position.set(pos.x, pos.y, pos.z)
+      meshGroup.position.set(pos.x, pos.y, pos.z)
       rotate(-Math.PI / 4)
 
       console.log(
-        `adding a new branch to the tree. pos:[${pos.x},${pos.y},${pos.z}]`,
+        `adding a new branch to the tree. pos:[${pos.x},${pos.y},${pos.z}] length:${branchData.length} `,
       )
     }
 
@@ -68,17 +71,17 @@ export const threeGenerator = (scene: THREE.Scene, branchData: BranchData) => {
       newData.pos.start.vec,
       newData.pos.end.vec,
     ))
-    // newData.pos.end.vec = new THREE.Vector3(3, 3, 3)
+
     newData.length = branchData.length / 2
     newData.angle = branchData.angle
     newData.depth -= 1.0
 
-    func(newData, mesh)
+    func(newData, meshGroup)
 
     function rotate(angle: number) {
       const quaternion = new THREE.Quaternion()
       quaternion.setFromAxisAngle(new THREE.Vector3(0, 0, 1), angle)
-      mesh.applyQuaternion(quaternion)
+      meshGroup.applyQuaternion(quaternion)
     }
   }
 
